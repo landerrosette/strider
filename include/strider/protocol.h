053c617435f8bb83a975596fc449578a96bb307f
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-06-07 05:13:02 +0800

    feat: add netlink protocol definitions

---
+++ include/strider/protocol.h
@@ -0,0 +1,27 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+
+#define STRIDER_GENL_FAMILY_NAME "strider"
+#define STRIDER_GENL_VERSION 1
+
+enum strider_genl_commands {
+    STRIDER_CMD_UNSPEC,
+    STRIDER_CMD_ADD_RULE,
+    STRIDER_CMD_DEL_RULE,
+    __STRIDER_CMD_MAX,
+};
+
+#define STRIDER_CMD_MAX (__STRIDER_CMD_MAX - 1)
+
+enum strider_genl_attrs {
+    STRIDER_NLA_UNSPEC,
+    STRIDER_NLA_KEYWORD,
+    STRIDER_NLA_ACTION,
+    __STRIDER_NLA_MAX,
+};
+
+#define STRIDER_NLA_MAX (__STRIDER_NLA_MAX - 1)
+
+
+#endif //PROTOCOL_H
