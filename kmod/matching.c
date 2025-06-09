f757ffb5f6f7c826b2c60bbe2df46580c7a9c599
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-06-10 07:47:34 +0800

    feat: initial implementation of matching

---
+++ kmod/matching.c
@@ -0,0 +1,132 @@
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include "matching.h"
+
+#include <linux/kernel.h>
+#include <linux/limits.h>
+#include <linux/list.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/rcupdate.h>
+#include <linux/slab.h>
+#include <linux/string.h>
+#include <strider/protocol.h>
+
+#define STRIDER_VERDICT_HIGHEST_PRECEDENCE 0
+#define STRIDER_VERDICT_LOWEST_PRECEDENCE INT_MAX
+
+struct strider_rule {
+    struct list_head list;
+    struct rcu_head rcu;
+
+    u8 action;
+    char keyword[]; // flexible array member
+};
+
+static LIST_HEAD(strider_rules_list);
+
+static DEFINE_MUTEX(strider_rules_list_lock); // lock to protect write access
+
+static void strider_rule_free_rcu_callback(struct rcu_head *rcu) {
+    struct strider_rule *rule = container_of(rcu, struct strider_rule, rcu);
+    kfree(rule);
+}
+
+static inline int get_verdict_precedence(enum strider_verdict verdict) {
+    switch (verdict) {
+        case STRIDER_VERDICT_DROP:
+            return STRIDER_VERDICT_HIGHEST_PRECEDENCE;
+        case STRIDER_VERDICT_ACCEPT:
+            return STRIDER_VERDICT_HIGHEST_PRECEDENCE + 1;
+        case STRIDER_VERDICT_NOMATCH:
+            return STRIDER_VERDICT_LOWEST_PRECEDENCE;
+    }
+    return STRIDER_VERDICT_LOWEST_PRECEDENCE; // should not happen
+}
+
+int strider_matching_init(void) {
+    // The list head and mutex are statically initialized.
+    // Nothing to do here for now.
+    return 0;
+}
+
+void strider_matching_exit(void) {
+    // When the module exits, there are no more readers.
+    // We can safely delete without waiting for a grace period.
+    // However, we must first acquire the mutex to prevent any last-minute writes from racing with our cleanup.
+    mutex_lock(&strider_rules_list_lock);
+
+    struct strider_rule *rule, *tmp;
+    list_for_each_entry_safe(rule, tmp, &strider_rules_list, list) {
+        list_del(&rule->list);
+        call_rcu(&rule->rcu, strider_rule_free_rcu_callback);
+    }
+
+    mutex_unlock(&strider_rules_list_lock);
+}
+
+int strider_matching_add_rule(const char *keyword, u8 action) {
+    struct strider_rule *rule = kmalloc(sizeof(*rule) + strlen(keyword) + 1, GFP_KERNEL);
+    if (!rule)
+        return -ENOMEM;
+    rule->action = action;
+    strcpy(rule->keyword, keyword);
+
+    mutex_lock(&strider_rules_list_lock);
+    list_add_rcu(&rule->list, &strider_rules_list);
+    mutex_unlock(&strider_rules_list_lock);
+
+    return 0;
+}
+
+int strider_matching_del_rule(const char *keyword, u8 action) {
+    mutex_lock(&strider_rules_list_lock);
+
+    struct strider_rule *rule, *victim = NULL;
+    list_for_each_entry(rule, &strider_rules_list, list) {
+        if (strcmp(rule->keyword, keyword) == 0 && rule->action == action) {
+            victim = rule;
+            break;
+        }
+    }
+    if (victim) list_del_rcu(&victim->list);
+
+    mutex_unlock(&strider_rules_list_lock);
+
+    if (victim) call_rcu(&victim->rcu, strider_rule_free_rcu_callback); // schedule the actual memory free
+
+    return victim ? 0 : -ENOENT;
+}
+
+enum strider_verdict strider_matching_packet(struct sk_buff *skb) {
+    const char *payload = "this is a test payload with bad-keyword inside";
+
+    enum strider_verdict final_verdict = STRIDER_VERDICT_NOMATCH;
+
+    rcu_read_lock();
+
+    struct strider_rule *rule;
+    list_for_each_entry_rcu(rule, &strider_rules_list, list) {
+        if (strstr(payload, rule->keyword)) {
+            enum strider_verdict current_verdict;
+            switch (rule->action) {
+                case STRIDER_ACTION_DROP:
+                    current_verdict = STRIDER_VERDICT_DROP;
+                    break;
+                case STRIDER_ACTION_ACCEPT:
+                    current_verdict = STRIDER_VERDICT_ACCEPT;
+                    break;
+                default:
+                    continue;
+            }
+            if (get_verdict_precedence(current_verdict) < get_verdict_precedence(final_verdict))
+                final_verdict = current_verdict;
+            if (get_verdict_precedence(final_verdict) == STRIDER_VERDICT_HIGHEST_PRECEDENCE)
+                break; // highest precedence reached, no need to check further
+        }
+    }
+
+    rcu_read_unlock();
+
+    return final_verdict;
+}
--- kmod/matching.h
+++ kmod/matching.h
@@ -5,6 +5,12 @@
 #include <linux/skbuff.h>
 #include <linux/types.h>
 
+enum strider_verdict {
+    STRIDER_VERDICT_DROP,
+    STRIDER_VERDICT_ACCEPT,
+    STRIDER_VERDICT_NOMATCH,
+};
+
 int strider_matching_init(void);
 
 void strider_matching_exit(void);
@@ -13,7 +19,7 @@ int strider_matching_add_rule(const char *keyword, u8 action);
 
 int strider_matching_del_rule(const char *keyword, u8 action);
 
-u8 strider_matching_packet(struct sk_buff *skb);
+enum strider_verdict strider_matching_packet(struct sk_buff *skb);
 
 
 #endif //STRIDER_KERNEL_MATCHING_H
