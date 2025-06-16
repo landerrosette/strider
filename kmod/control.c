edb7a009b8b32048008654573088a0e26597f894
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-06-16 20:22:22 +0800

    fix:

--- kmod/control.c
+++ kmod/control.c
@@ -84,6 +84,6 @@ int __init strider_control_init(void) {
     return ret;
 }
 
-void __init strider_control_exit(void) {
+void __exit strider_control_exit(void) {
     genl_unregister_family(&strider_genl_family);
 }
