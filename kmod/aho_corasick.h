07eae3a11c470057dc65c9ee87eba4b5956883eb
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-06-17 16:36:03 +0800

    feat: add Aho-Corasick automaton interface

--- kmod/aho_corasick.h
+++ kmod/aho_corasick.h
@@ -1,4 +1,26 @@
-#ifndef STRIDER_KMOD_AHO_CORASICK_H
-#define STRIDER_KMOD_AHO_CORASICK_H
+#ifndef AHO_CORASICK_H
+#define AHO_CORASICK_H
 
-#endif //STRIDER_KMOD_AHO_CORASICK_H
+
+#include <linux/list.h>
+#include <linux/skbuff.h>
+
+struct ac_automaton;
+
+struct ac_rule {
+    struct list_head list;
+
+    const char *pattern;
+    size_t len;
+    void *priv;
+};
+
+struct ac_automaton * __must_check ac_automaton_build(struct list_head *head);
+
+void ac_automaton_free(struct ac_automaton *automaton);
+
+int ac_automaton_match(struct ac_automaton *automaton, const struct sk_buff *skb, size_t offset, size_t len,
+                       int (*cb)(void *priv, size_t offset, void *cb_ctx), void *cb_ctx);
+
+
+#endif //AHO_CORASICK_H
