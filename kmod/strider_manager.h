643506dca1b6d2497c34cd0e69db5b945ba05782
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-07-29 11:18:35 +0800

    refactor: add strider_manager.h

---
+++ kmod/strider_manager.h
@@ -0,0 +1,28 @@
+#ifndef STRIDER_MANAGER_H
+#define STRIDER_MANAGER_H
+
+
+#include <linux/init.h>
+
+#include "strider_ac.h"
+
+struct strider_set;
+
+int __init strider_manager_init(void);
+
+void strider_manager_cleanup(void);
+
+int strider_set_create(const char *name);
+
+int strider_set_destroy(const char *name);
+
+int strider_set_add_pattern(const char *set_name, const char *pattern);
+
+int strider_set_del_pattern(const char *set_name, const char *pattern);
+
+struct strider_set *strider_set_find_rcu(const char *name);
+
+const struct strider_ac_automaton *strider_set_get_automaton(const struct strider_set *set);
+
+
+#endif //STRIDER_MANAGER_H
