545adb0a815580b3a7d5352e343484e3a66c4d9d
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-08-06 19:41:18 +0800

    feat: implement strider_set_destroy, _del_pattern and _manager_cleanup

--- kmod/strider_manager.c
+++ kmod/strider_manager.c
@@ -55,18 +55,17 @@ static int strider_set_rebuild_automaton_locked(struct strider_set *set) {
     const struct strider_pattern_entry *entry;
     list_for_each_entry(entry, &set->patterns, list)
         ++num_patterns;
-
     const char **patterns_array = vmalloc(num_patterns * sizeof(*patterns_array));
     if (!patterns_array)
         return -ENOMEM;
     size_t i = 0;
     list_for_each_entry(entry, &set->patterns, list)
         patterns_array[i++] = entry->pattern;
+
     struct strider_ac_automaton *new_automaton = strider_ac_automaton_compile(patterns_array, num_patterns);
     vfree(patterns_array);
     if (IS_ERR(new_automaton))
         return PTR_ERR(new_automaton);
-
     struct strider_ac_automaton *old_automaton = rcu_replace_pointer(set->automaton, new_automaton, lockdep_is_held(&set->lock));
     if (old_automaton)
         strider_ac_automaton_destroy_rcu(old_automaton);
@@ -74,70 +73,18 @@ static int strider_set_rebuild_automaton_locked(struct strider_set *set) {
     return 0;
 }
 
-// // This function acts as a central policy decision point for rule precedence.
-// // By encapsulating this logic, it allows for future extensions, such as configurable precedence.
-// // A lower return value signifies a higher precedence.
-// static __attribute_const__ int strider_get_verdict_precedence(enum strider_verdict verdict) {
-//     switch (verdict) {
-//         case STRIDER_VERDICT_DROP:
-//             return STRIDER_VERDICT_HIGHEST_PRECEDENCE;
-//         case STRIDER_VERDICT_ACCEPT:
-//             return STRIDER_VERDICT_HIGHEST_PRECEDENCE + 1;
-//         case STRIDER_VERDICT_NOMATCH:
-//             return STRIDER_VERDICT_LOWEST_PRECEDENCE;
-//     }
-//     // should not reach here
-//     WARN_ON_ONCE(1);
-//     return STRIDER_VERDICT_LOWEST_PRECEDENCE;
-// }
+// MUST be called with set->lock held
+static void strider_set_deinit_locked(struct strider_set *set) {
+    struct strider_pattern_entry *entry, *tmp;
+    list_for_each_entry_safe(entry, tmp, &set->patterns, list) {
+        list_del(&entry->list);
+        kfree(entry);
+    }
 
-// static int strider_get_l4_payload_coords(const struct sk_buff *skb, size_t *offset, size_t *len) {
-//     const struct iphdr *iph = ip_hdr(skb);
-//     unsigned int ip_hdr_len = iph->ihl * 4;
-
-//     if (unlikely(ip_hdr_len < sizeof(struct iphdr)))
-//         return -EINVAL;
-
-//     if (iph->protocol == IPPROTO_TCP) {
-//         struct tcphdr _tcph;
-//         const struct tcphdr *tcph = skb_header_pointer(skb, ip_hdr_len, sizeof(_tcph), &_tcph);
-//         if (unlikely(!tcph))
-//             return -EINVAL;
-//         unsigned int tcp_hdr_len = tcph->doff * 4;
-
-//         // TCP header length must be at least its minimum fixed size.
-//         if (unlikely(tcp_hdr_len < sizeof(struct tcphdr)))
-//             return -EINVAL;
-//         // The combined IP and TCP header lengths must not exceed the total length of the IP datagram.
-//         if (unlikely(ip_hdr_len + tcp_hdr_len > ntohs(iph->tot_len)))
-//             return -EINVAL;
-
-//         *offset = ip_hdr_len + tcp_hdr_len;
-//         *len = ntohs(iph->tot_len) - *offset;
-//     } else if (iph->protocol == IPPROTO_UDP) {
-//         struct udphdr _udph;
-//         const struct udphdr *udph = skb_header_pointer(skb, ip_hdr_len, sizeof(_udph), &_udph);
-//         if (unlikely(!udph))
-//             return -EINVAL;
-
-//         // UDP length field must be at least the size of the header.
-//         if (unlikely(ntohs(udph->len) < sizeof(struct udphdr)))
-//             return -EINVAL;
-//         // The UDP-claimed length must not exceed the available space calculated from the IP header.
-//         if (unlikely(ntohs(udph->len) > ntohs(iph->tot_len) - ip_hdr_len))
-//             return -EINVAL;
-
-//         *offset = ip_hdr_len + sizeof(struct udphdr);
-//         *len = ntohs(udph->len) - sizeof(struct udphdr);
-//     } else {
-//         return -EPROTONOSUPPORT;
-//     }
-
-//     if (unlikely(*offset > skb->len || *len > skb->len - *offset))
-//         return -EINVAL;
-
-//     return 0;
-// }
+    struct strider_ac_automaton *automaton = rcu_dereference_protected(set->automaton, lockdep_is_held(&set->lock));
+    if (automaton)
+        strider_ac_automaton_destroy_rcu(automaton);
+}
 
 int __init strider_manager_init(void) {
     // Nothing to do here as the hash table is statically initialized.
@@ -145,22 +92,19 @@ int __init strider_manager_init(void) {
 }
 
 void strider_manager_cleanup(void) {
-//     mutex_lock(&strider_rules_list_lock);
-
-//     struct strider_rule *rule, *tmp;
-//     list_for_each_entry_safe(rule, tmp, &strider_rules_list, list) {
-//         list_del(&rule->list);
-//         kfree(rule);
-//     }
-
-//     struct strider_ac_rcu *wrapper = rcu_replace_pointer(strider_ac_automaton, NULL,
-//                                                          lockdep_is_held(&strider_rules_list_lock));
-//     if (wrapper)
-//         call_rcu(&wrapper->rcu, strider_ac_automaton_free_rcu_cb);
-
-//     mutex_unlock(&strider_rules_list_lock);
-
-//     rcu_barrier();
+    mutex_lock(&strider_sets_ht_lock);
+    struct strider_set *set;
+    struct hlist_node *tmp;
+    int bkt;
+    hash_for_each_safe(strider_sets_ht, bkt, tmp, set, node) {
+        mutex_lock(&set->lock);
+        hash_del_rcu(&set->node);
+        strider_set_deinit_locked(set);
+        mutex_unlock(&set->lock);
+        kfree_rcu(set, rcu);
+    }
+    mutex_unlock(&strider_sets_ht_lock);
+    rcu_barrier();
 }
 
 int strider_set_create(const char *name) {
@@ -176,7 +120,6 @@ int strider_set_create(const char *name) {
     mutex_init(&new_set->lock);
 
     mutex_lock(&strider_sets_ht_lock);
-
     struct strider_set *set = strider_set_lookup_locked(name);
     if (set) {
         ret = -EEXIST;
@@ -196,27 +139,18 @@ fail:
 
 int strider_set_destroy(const char *name) {
     mutex_lock(&strider_sets_ht_lock);
-
-    struct strider_set *set, *tmp;
-    int ret = -ENOENT;
-//     list_for_each_entry_safe(rule, tmp, &strider_rules_list, list) {
-//         if (strcmp(rule->pattern, pattern) == 0 && rule->action == action) {
-//             list_del(&rule->list);
-//             ret = strider_ac_automaton_rebuild_locked();
-//             if (ret < 0) {
-//                 // rollback
-//                 list_add(&rule->list, &strider_rules_list);
-//                 goto out;
-//             }
-//             kfree(rule);
-//             break;
-//         }
-//     }
-
-out:
+    struct strider_set *set = strider_set_lookup_locked(name);
+    if (!set) {
+        mutex_unlock(&strider_sets_ht_lock);
+        return -ENOENT;
+    }
+    mutex_lock(&set->lock);
+    hash_del_rcu(&set->node);
     mutex_unlock(&strider_sets_ht_lock);
-
-    return ret;
+    strider_set_deinit_locked(set);
+    mutex_unlock(&set->lock);
+    kfree_rcu(set, rcu);
+    return 0;
 }
 
 int strider_set_add_pattern(const char *set_name, const char *pattern) {
@@ -230,16 +164,13 @@ int strider_set_add_pattern(const char *set_name, const char *pattern) {
     strscpy(new_entry->pattern, pattern, strlen(pattern) + 1);
 
     mutex_lock(&strider_sets_ht_lock);
-
     struct strider_set *set = strider_set_lookup_locked(set_name);
     if (!set) {
         ret = -ENOENT;
         goto fail_sets_ht_unlock;
     }
-
     mutex_lock(&set->lock);
     mutex_unlock(&strider_sets_ht_lock);
-
     const struct strider_pattern_entry *entry;
     list_for_each_entry(entry, &set->patterns, list) {
         if (strcmp(entry->pattern, pattern) == 0) {
@@ -248,13 +179,11 @@ int strider_set_add_pattern(const char *set_name, const char *pattern) {
         }
     }
     list_add(&new_entry->list, &set->patterns);
-
     ret = strider_set_rebuild_automaton_locked(set);
     if (ret < 0) {
         list_del(&new_entry->list);
         goto fail_set_unlock;
     }
-
     mutex_unlock(&set->lock);
 
 out:
@@ -270,9 +199,44 @@ fail:
     goto out;
 }
 
-// struct strider_match_ctx {
-//     enum strider_verdict verdict;
-// };
+int strider_set_del_pattern(const char *set_name, const char *pattern) {
+    int ret = 0;
+
+    mutex_lock(&strider_sets_ht_lock);
+    struct strider_set *set = strider_set_lookup_locked(set_name);
+    if (!set) {
+        ret = -ENOENT;
+        goto fail;
+    }
+    mutex_lock(&set->lock);
+    mutex_unlock(&strider_sets_ht_lock);
+    ret = -ENOENT;
+    struct strider_pattern_entry *entry, *tmp;
+    list_for_each_entry_safe(entry, tmp, &set->patterns, list) {
+        if (strcmp(entry->pattern, pattern) == 0) {
+            list_del(&entry->list);
+            ret = strider_set_rebuild_automaton_locked(set);
+            if (ret < 0) {
+                // rollback
+                list_add(&entry->list, &set->patterns);
+                goto fail_set_unlock;
+            }
+            kfree(entry);
+            break;
+        }
+    }
+    mutex_unlock(&set->lock);
+
+out:
+    return ret;
+
+fail_set_unlock:
+    mutex_unlock(&set->lock);
+    goto out;
+fail:
+    mutex_unlock(&strider_sets_ht_lock);
+    goto out;
+}
 
 // static int strider_match_cb(const void *priv, size_t offset, void *cb_ctx) {
 //     const struct strider_rule *rule = priv;
