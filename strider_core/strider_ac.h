b75c901b087acf3fd2faeaf9e4b6cc555ff6872e
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-08-13 10:49:27 +0800

    refactor: refactor strider_ac's interface

--- strider_core/strider_ac.h
+++ strider_core/strider_ac.h
@@ -2,43 +2,25 @@
 #define STRIDER_AC_H
 
 
-#include <linux/compiler.h>
 #include <linux/types.h>
 
-struct strider_ac_automaton;
-
-struct ac_node;
+struct strider_ac;
 
 struct strider_ac_match_state {
-    const struct ac_node *cursor;
-    const struct strider_ac_automaton *automaton;
-    size_t stream_pos; // position in the logical input stream
+    const void *cursor;
 };
 
-struct strider_ac_automaton * __cold __must_check strider_ac_automaton_compile(const char *const *patterns, size_t num_patterns);
+struct strider_ac *strider_ac_init(gfp_t gfp_mask);
 
-/**
- * strider_ac_automaton_destroy() - Destroy an automaton synchronously.
- */
-void __cold strider_ac_automaton_destroy(struct strider_ac_automaton *automaton);
+int strider_ac_add_pattern(struct strider_ac *ac, const u8 *pattern, size_t len, gfp_t gfp_mask);
 
-/**
- * strider_ac_automaton_destroy_rcu() - Schedule the destruction of an automaton via RCU.
- */
-void __cold strider_ac_automaton_destroy_rcu(struct strider_ac_automaton *automaton);
+int strider_ac_compile(struct strider_ac *ac, gfp_t gfp_mask);
 
-void strider_ac_match_state_init(struct strider_ac_match_state *state, const struct strider_ac_automaton *automaton);
+void strider_ac_destroy(struct strider_ac *ac);
 
-/**
- * strider_ac_automaton_scan()
- *
- * Return: 0 on successful processing of all data.
- *         Otherwise, returns a non-zero value propagated from the callback, stopping further processing.
- *         Conventionally, negative values indicate errors (e.g., -ENOMEM),
- *         while positive values signal other non-error conditions for stopping (e.g., a specific match found).
- */
-int strider_ac_automaton_scan(struct strider_ac_match_state *state, const u8 *data, size_t len,
-                              int (*cb)(void *cb_ctx), void *cb_ctx);
+void strider_ac_match_init(const struct strider_ac *ac, struct strider_ac_match_state *state);
+
+bool strider_ac_match_next(struct strider_ac_match_state *state, const u8 *data, size_t len);
 
 
 #endif //STRIDER_AC_H
