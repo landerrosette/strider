22535c8509482b9b834abdeb3d372d8f8c1d133b
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-08-18 01:39:48 +0800

    refactor: refactor strider_manager's interface

--- strider_core/strider_manager.h
+++ strider_core/strider_manager.h
@@ -2,28 +2,34 @@
 #define STRIDER_MANAGER_H
 
 
-#include <linux/init.h>
-#include <linux/compiler.h>
+#include <linux/mutex.h>
+#include <linux/refcount.h>
+#include <linux/types.h>
+#include <strider/limits.h>
 
-struct strider_set;
+struct strider_ac;
 
-struct strider_ac_automaton;
+struct strider_pattern_entry {
+    struct list_head list;
+    char pattern[];
+};
 
-int __init strider_manager_init(void);
+struct strider_set {
+    struct strider_ac __rcu *ac;
+    struct mutex lock;
+    struct hlist_node node;
+    refcount_t refcount;
+    struct list_head patterns;
+    char name[STRIDER_SET_NAME_MAX_LEN + 1];
+};
 
-void __cold strider_manager_cleanup(void);
+int strider_set_create(const char *name);
 
-int __cold strider_set_create(const char *name);
+int strider_set_destroy(const char *name);
 
-int __cold strider_set_destroy(const char *name);
+int strider_set_add_pattern(const char *set_name, const u8 *pattern, size_t len);
 
-int __cold strider_set_add_pattern(const char *set_name, const char *pattern);
-
-int __cold strider_set_del_pattern(const char *set_name, const char *pattern);
-
-const struct strider_set *strider_set_lookup_rcu(const char *name);
-
-const struct strider_ac_automaton *strider_get_automaton(const struct strider_set *set);
+int strider_set_del_pattern(const char *set_name, const u8 *pattern, size_t len);
 
 
 #endif //STRIDER_MANAGER_H
