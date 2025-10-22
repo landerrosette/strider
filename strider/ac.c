90ac459fc7915148bff85b8d2dff26a49172f337
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-10-22 15:15:10 +0800

    refactor(ac): accelerate vacant search

--- strider/ac.c
+++ strider/ac.c
@@ -1,14 +1,18 @@
 #include "ac.h"
 
+#include <linux/bitmap.h>
 #include <linux/container_of.h>
 #include <linux/err.h>
 #include <linux/errno.h>
+#include <linux/limits.h>
 #include <linux/list.h>
 #include <linux/list_sort.h>
 #include <linux/overflow.h>
 #include <linux/rcupdate.h>
 #include <linux/slab.h>
 #include <linux/types.h>
+
+#define STRIDER_AC_ROOT_STATE_ID 1
 
 struct strider_ac_output {
     struct list_head list;
@@ -32,8 +36,8 @@ struct strider_ac_transition {
 
 struct strider_ac_trie {
     struct strider_ac_node *root;
-    size_t num_nodes;
-    u32 max_state_id;
+    u32 num_nodes;
+    u32 max_base_val;
 };
 
 struct strider_ac {
@@ -41,9 +45,8 @@ struct strider_ac {
     u32 *check;
     u32 *failures;
     struct list_head *outputs; // array of lists of struct strider_ac_output
-    size_t arr_size;
+    u32 arr_size;
     struct rcu_head rcu;
-    u8 data[];
 };
 
 static struct strider_ac_node *strider_ac_node_create(void) {
@@ -114,7 +117,8 @@ static int strider_ac_trie_add_targets(struct strider_ac_trie *trie,
                 child = strider_ac_node_add_child(node, target->pattern[i]);
                 if (!child)
                     return -ENOMEM;
-                ++trie->num_nodes;
+                if (check_add_overflow(trie->num_nodes, 1, &trie->num_nodes) != 0)
+                    return -ENOMEM;
             }
             node = child;
         }
@@ -158,20 +162,6 @@ static void strider_ac_trie_link_failures(struct strider_ac_trie *trie) {
     }
 }
 
-static void strider_ac_trie_reset_state_ids(struct strider_ac_trie *trie) {
-    LIST_HEAD(queue);
-    list_add_tail(&trie->root->list, &queue);
-    while (!list_empty(&queue)) {
-        struct strider_ac_node *node = list_first_entry(&queue, struct strider_ac_node, list);
-        list_del(&node->list);
-        node->state_id = 0;
-        const struct strider_ac_transition *tsn;
-        list_for_each_entry(tsn, &node->transitions, list)
-            list_add_tail(&tsn->next->list, &queue);
-    }
-    trie->max_state_id = 0;
-}
-
 static int strider_ac_transition_cmp(void *priv, const struct list_head *a, const struct list_head *b) {
     const struct strider_ac_transition *tsn_a = list_entry(a, struct strider_ac_transition, list);
     const struct strider_ac_transition *tsn_b = list_entry(b, struct strider_ac_transition, list);
@@ -179,53 +169,68 @@ static int strider_ac_transition_cmp(void *priv, const struct list_head *a, cons
 }
 
 static int strider_ac_trie_assign_state_ids(struct strider_ac_trie *trie) {
-    size_t num_states = trie->num_nodes;
-    size_t arr_size = num_states;
-
-retry_arr_size:
-    arr_size = size_mul(arr_size, 2);
-    if (arr_size == SIZE_MAX)
+    u32 arr_size;
+    if (check_add_overflow(trie->num_nodes, 256, &arr_size) != 0)
         return -ENOMEM;
-    u32 *check = kvcalloc(arr_size, sizeof(*check), GFP_KERNEL);
-    if (!check)
+    unsigned long *occupied = bitmap_zalloc(arr_size, GFP_KERNEL);
+    if (!occupied)
         return -ENOMEM;
 
-    trie->root->state_id = 1;
+    trie->root->state_id = STRIDER_AC_ROOT_STATE_ID;
+    set_bit(STRIDER_AC_ROOT_STATE_ID, occupied);
     LIST_HEAD(stack);
     list_add(&trie->root->list, &stack);
+
     while (!list_empty(&stack)) {
         struct strider_ac_node *node = list_first_entry(&stack, struct strider_ac_node, list);
         list_del(&node->list);
-
+        if (list_empty(&node->transitions))
+            continue;
         list_sort(NULL, &node->transitions, strider_ac_transition_cmp);
 
-        u32 base_val = 1;
-    retry_base_val:;
+        u8 first_byte = list_first_entry(&node->transitions, struct strider_ac_transition, list)->byte;
+        u8 last_byte = list_last_entry(&node->transitions, struct strider_ac_transition, list)->byte;
+        u32 slot = first_byte + 1;
+    retry:
+        slot = find_next_zero_bit(occupied, arr_size, slot); // arr_size shall be >= 256 so base_val will not underflow
+        u32 base_val = slot - first_byte;
+        if (base_val + last_byte >= arr_size) {
+            u32 new_arr_size;
+            if (check_mul_overflow(arr_size, 2, &new_arr_size) != 0) {
+                bitmap_free(occupied);
+                return -ENOMEM;
+            }
+            unsigned long *new_occupied = bitmap_zalloc(new_arr_size, GFP_KERNEL);
+            if (!new_occupied) {
+                bitmap_free(occupied);
+                return -ENOMEM;
+            }
+            bitmap_copy(new_occupied, occupied, arr_size);
+            bitmap_free(occupied);
+            occupied = new_occupied;
+            arr_size = new_arr_size;
+            goto retry;
+        }
         const struct strider_ac_transition *tsn;
         list_for_each_entry(tsn, &node->transitions, list) {
-            if (base_val + tsn->byte >= arr_size) {
-                strider_ac_trie_reset_state_ids(trie);
-                kvfree(check);
-                goto retry_arr_size;
-            }
-            if (check[base_val + tsn->byte] != 0) {
-                ++base_val;
-                goto retry_base_val;
+            if (test_bit(base_val + tsn->byte, occupied)) {
+                ++slot;
+                goto retry;
             }
         }
 
         node->base_val = base_val;
+        if (node->base_val > trie->max_base_val)
+            trie->max_base_val = node->base_val;
         list_for_each_entry(tsn, &node->transitions, list) {
             struct strider_ac_node *child = tsn->next;
             child->state_id = base_val + tsn->byte;
-            check[child->state_id] = node->state_id;
+            set_bit(child->state_id, occupied);
             list_add(&child->list, &stack);
         }
-        if (node->state_id > trie->max_state_id)
-            trie->max_state_id = node->state_id;
     }
 
-    kvfree(check);
+    bitmap_free(occupied);
     return 0;
 }
 
@@ -239,51 +244,55 @@ struct strider_ac *strider_ac_build(const struct strider_ac_target *(*get_target
         return ERR_PTR(-ENOMEM);
     }
     trie->root = root;
-    ++trie->num_nodes;
+    trie->num_nodes = 1;
     int ret = strider_ac_trie_add_targets(trie, get_target, iter_ctx);
-    if (ret < 0) {
-        strider_ac_trie_destroy(trie);
-        return ERR_PTR(ret);
-    }
+    if (ret < 0)
+        goto fail;
     strider_ac_trie_link_failures(trie);
     ret = strider_ac_trie_assign_state_ids(trie);
-    if (ret < 0) {
-        strider_ac_trie_destroy(trie);
-        return ERR_PTR(ret);
-    }
+    if (ret < 0)
+        goto fail;
 
-    size_t arr_size = trie->max_state_id + 1;
-    // Make sure arr_size is large enough so that
-    // the bounds check in the match function is predictable for the root state.
-    // For any byte c (0-255), we want base[1] + c < arr_size.
-    // This avoids frequent branch mispredictions on worst case input.
-    if (arr_size < trie->root->base_val + 256)
-        arr_size = trie->root->base_val + 256;
-    size_t ac_data_size = array3_size(3, arr_size, sizeof(u32));                           // base, check, failures
-    ac_data_size = size_add(ac_data_size, array_size(arr_size, sizeof(struct list_head))); // outputs
-    if (ac_data_size == SIZE_MAX) {
-        strider_ac_trie_destroy(trie);
-        return ERR_PTR(-ENOMEM);
+    u32 arr_size;
+    // make arrays large enough to eliminate match-time bounds check
+    if (check_add_overflow(trie->max_base_val, 256, &arr_size) != 0) {
+        ret = -ENOMEM;
+        goto fail;
     }
-    struct strider_ac *ac = kvzalloc(struct_size(ac, data, ac_data_size), GFP_KERNEL);
+    struct strider_ac *ac = kmalloc(sizeof(*ac), GFP_KERNEL);
     if (!ac) {
-        strider_ac_trie_destroy(trie);
-        return ERR_PTR(-ENOMEM);
+        ret = -ENOMEM;
+        goto fail;
     }
-    ac->arr_size = arr_size;
-    ac->base = (u32 *) ac->data;
-    ac->check = ac->base + ac->arr_size;
-    ac->failures = ac->check + ac->arr_size;
-    ac->outputs = (struct list_head *) (ac->failures + ac->arr_size);
-    for (size_t i = 0; i < ac->arr_size; ++i)
+    ac->base = kvcalloc(arr_size, sizeof(*ac->base), GFP_KERNEL);
+    if (!ac->base) {
+        ret = -ENOMEM;
+        goto fail_kfree_ac;
+    }
+    ac->check = kvcalloc(arr_size, sizeof(*ac->check), GFP_KERNEL);
+    if (!ac->check) {
+        ret = -ENOMEM;
+        goto fail_kvfree_base;
+    }
+    ac->failures = kvcalloc(arr_size, sizeof(*ac->failures), GFP_KERNEL);
+    if (!ac->failures) {
+        ret = -ENOMEM;
+        goto fail_kvfree_check;
+    }
+    ac->outputs = kvcalloc(arr_size, sizeof(*ac->outputs), GFP_KERNEL);
+    if (!ac->outputs) {
+        ret = -ENOMEM;
+        goto fail_kvfree_failures;
+    }
+    for (u32 i = 0; i < arr_size; ++i)
         INIT_LIST_HEAD(&ac->outputs[i]);
+    ac->arr_size = arr_size;
 
     LIST_HEAD(queue);
     list_add_tail(&trie->root->list, &queue);
     while (!list_empty(&queue)) {
         struct strider_ac_node *node = list_first_entry(&queue, struct strider_ac_node, list);
         list_del(&node->list);
-
         ac->base[node->state_id] = node->base_val;
         ac->failures[node->state_id] = node->failure->state_id;
         list_replace_init(&node->outputs, &ac->outputs[node->state_id]); // steal list of outputs
@@ -298,17 +307,33 @@ struct strider_ac *strider_ac_build(const struct strider_ac_target *(*get_target
 
     strider_ac_trie_destroy(trie);
     return ac;
+
+fail_kvfree_failures:
+    kvfree(ac->failures);
+fail_kvfree_check:
+    kvfree(ac->check);
+fail_kvfree_base:
+    kvfree(ac->base);
+fail_kfree_ac:
+    kfree(ac);
+fail:
+    strider_ac_trie_destroy(trie);
+    return ERR_PTR(ret);
 }
 
 static void strider_ac_destroy(struct strider_ac *ac) {
-    for (size_t i = 0; i < ac->arr_size; ++i) {
+    for (u32 i = 0; i < ac->arr_size; ++i) {
         struct strider_ac_output *out, *tmp;
         list_for_each_entry_safe(out, tmp, &ac->outputs[i], list) {
             list_del(&out->list);
             kfree(out);
         }
     }
-    kvfree(ac);
+    kvfree(ac->outputs);
+    kvfree(ac->failures);
+    kvfree(ac->check);
+    kvfree(ac->base);
+    kfree(ac);
 }
 
 static void strider_ac_destroy_rcu_cb(struct rcu_head *rcu) {
@@ -322,7 +347,7 @@ void strider_ac_schedule_destroy(struct strider_ac *ac) {
 
 void strider_ac_match_init(const struct strider_ac *ac, struct strider_ac_match_state *state) {
     state->ac = ac;
-    state->ac_state = 1;
+    state->ac_state = STRIDER_AC_ROOT_STATE_ID;
 }
 
 int strider_ac_match(struct strider_ac_match_state *state, const u8 *data, size_t len,
@@ -333,15 +358,15 @@ int strider_ac_match(struct strider_ac_match_state *state, const u8 *data, size_
     for (size_t i = 0; i < len; ++i) {
         for (u32 next; ; ac_state = ac->failures[ac_state]) {
             next = ac->base[ac_state] + data[i];
-            if (next < ac->arr_size && ac->check[next] == ac_state) {
+            if (ac->check[next] == ac_state) {
                 ac_state = next;
                 break;
             }
-            if (ac_state == 1)
+            if (ac_state == STRIDER_AC_ROOT_STATE_ID)
                 break;
         }
 
-        for (u32 out = ac_state; out != 1; out = ac->failures[out]) {
+        for (u32 out = ac_state; out != STRIDER_AC_ROOT_STATE_ID; out = ac->failures[out]) {
             if (!list_empty(&ac->outputs[out])) {
                 const struct strider_ac_output *output;
                 list_for_each_entry(output, &ac->outputs[out], list) {
